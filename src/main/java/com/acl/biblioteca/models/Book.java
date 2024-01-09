67b3e900a5627aa23117dcba45640d3125ee2160
Sebaplz <sebastian.h.neira@outlook.com>
2024-01-09 18:00:24 -0300

    Se agrega createdAt y updatedAt en la clase book, se agrega swagger y su dependencia

--- src/main/java/com/acl/biblioteca/models/Book.java
+++ src/main/java/com/acl/biblioteca/models/Book.java
@@ -2,12 +2,18 @@ package com.acl.biblioteca.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Date;
 
 @Entity
 @Table(name = "books")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,10 +25,22 @@ public class Book {
     @Lob
     private String sinopsis;
     @Column(updatable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date createdAt;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date updatedAt;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     @JsonIgnore
     private User user;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
 }
